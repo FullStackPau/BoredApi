dcc59da35509d363091c7b3c88a384fee8a85d8d
PauStack <paulopesruis@gmail.com>
2021-01-17 00:08:21 +0100

    First Version

---
+++ src/components/Result.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+
+const Result = () => {
+    const [activity, setActivity] = useState('');
+
+    const data = async () => {
+        const resultdata = await axios.get("http://www.boredapi.com/api/activity/");
+        setActivity(resultdata.data.activity);
+        console.log(resultdata);
+    }
+
+    return ( 
+        <div className="result">
+            <h1>Random Activities</h1>
+            <div className="data">
+                <p>{activity}</p>
+            </div>
+            <button
+                onClick={data}
+            >Activities</button>
+        </div>
+     );
+}
+ 
+export default Result;
+
