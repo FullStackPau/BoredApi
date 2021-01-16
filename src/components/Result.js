4bd23dcdc1d39a94637f7b5caa6eff9f6964016a
PauStack <paulopesruis@gmail.com>
2021-01-17 00:16:43 +0100

    Fixed Empty Beginning

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
 
 const Result = () => {
     const [activity, setActivity] = useState('');
+
+    useEffect(() => {
+        data();
+    },[]);
 
     const data = async () => {
         const resultdata = await axios.get("http://www.boredapi.com/api/activity/");
