e0a988a7d046962945a925a951dcfdff241759f1
PauStack <paulopesruis@gmail.com>
2021-01-17 00:20:42 +0100

    Added Comments

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,10 +6,12 @@ import axios from 'axios';
 const Result = () => {
     const [activity, setActivity] = useState('');
 
+    //Only will execute one time
     useEffect(() => {
         data();
     },[]);
 
+    //That function make the connection to api using Axios.
     const data = async () => {
         const resultdata = await axios.get("http://www.boredapi.com/api/activity/");
         setActivity(resultdata.data.activity);
